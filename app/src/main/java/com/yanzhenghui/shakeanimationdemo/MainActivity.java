5c3ccc7e0cd21c89bac3be1c3f535421de13cd24
鄢正辉 <hzyanzhenghui@corp.netease.com>
2016-06-01 20:15:09 +0800

    init

---
+++ app/src/main/java/com/yanzhenghui/shakeanimationdemo/MainActivity.java
@@ -0,0 +1,126 @@
+package com.yanzhenghui.shakeanimationdemo;
+
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView mIvShake;
+    private AppCompatButton mBtnStart;
+    private RadioGroup mRgChooseAnim;
+
+    private boolean mUsingViewAnim = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mIvShake = (ImageView) findViewById(R.id.shake);
+        mBtnStart = (AppCompatButton) findViewById(R.id.start);
+        mRgChooseAnim = (RadioGroup) findViewById(R.id.choose_anim);
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startShakeAnim();
+            }
+        });
+        mRgChooseAnim.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.view_anim) {
+                    mUsingViewAnim = true;
+                } else {
+                    mUsingViewAnim = false;
+                }
+            }
+        });
+    }
+
+    private void startShakeAnim() {
+        if (mUsingViewAnim) {
+            startShakeByViewAnim(mIvShake, 0.9f, 1.1f, 10f, 1000);
+        } else {
+            startShakeByPropertyAnim(mIvShake, 0.9f, 1.1f, 10f, 1000);
+        }
+    }
+
+    private void startShakeByViewAnim(View view, float scaleSmall, float scaleLarge, float shakeDegrees, long duration) {
+        if (view == null) {
+            return;
+        }
+        //TODO 验证参数的有效性
+
+        //由小变大
+        Animation scaleAnim = new ScaleAnimation(scaleSmall, scaleLarge, scaleSmall, scaleLarge);
+        //从左向右
+        Animation rotateAnim = new RotateAnimation(-shakeDegrees, shakeDegrees, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+        scaleAnim.setDuration(duration);
+        rotateAnim.setDuration(duration / 10);
+        rotateAnim.setRepeatMode(Animation.REVERSE);
+        rotateAnim.setRepeatCount(10);
+
+        AnimationSet smallAnimationSet = new AnimationSet(false);
+        smallAnimationSet.addAnimation(scaleAnim);
+        smallAnimationSet.addAnimation(rotateAnim);
+
+        view.startAnimation(smallAnimationSet);
+    }
+
+    private void startShakeByPropertyAnim(View view, float scaleSmall, float scaleLarge, float shakeDegrees, long duration) {
+        if (view == null) {
+            return;
+        }
+        //TODO 验证参数的有效性
+
+        //先变小后变大
+        PropertyValuesHolder scaleXValuesHolder = PropertyValuesHolder.ofKeyframe(View.SCALE_X,
+                Keyframe.ofFloat(0f, 1.0f),
+                Keyframe.ofFloat(0.25f, scaleSmall),
+                Keyframe.ofFloat(0.5f, scaleLarge),
+                Keyframe.ofFloat(0.75f, scaleLarge),
+                Keyframe.ofFloat(1.0f, 1.0f)
+        );
+        PropertyValuesHolder scaleYValuesHolder = PropertyValuesHolder.ofKeyframe(View.SCALE_Y,
+                Keyframe.ofFloat(0f, 1.0f),
+                Keyframe.ofFloat(0.25f, scaleSmall),
+                Keyframe.ofFloat(0.5f, scaleLarge),
+                Keyframe.ofFloat(0.75f, scaleLarge),
+                Keyframe.ofFloat(1.0f, 1.0f)
+        );
+
+        //先往左再往右
+        PropertyValuesHolder rotateValuesHolder = PropertyValuesHolder.ofKeyframe(View.ROTATION,
+                Keyframe.ofFloat(0f, 0f),
+                Keyframe.ofFloat(0.1f, -shakeDegrees),
+                Keyframe.ofFloat(0.2f, shakeDegrees),
+                Keyframe.ofFloat(0.3f, -shakeDegrees),
+                Keyframe.ofFloat(0.4f, shakeDegrees),
+                Keyframe.ofFloat(0.5f, -shakeDegrees),
+                Keyframe.ofFloat(0.6f, shakeDegrees),
+                Keyframe.ofFloat(0.7f, -shakeDegrees),
+                Keyframe.ofFloat(0.8f, shakeDegrees),
+                Keyframe.ofFloat(0.9f, -shakeDegrees),
+                Keyframe.ofFloat(1.0f, 0f)
+        );
+
+        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(view, scaleXValuesHolder, scaleYValuesHolder, rotateValuesHolder);
+        objectAnimator.setDuration(duration);
+        objectAnimator.start();
+    }
+}
